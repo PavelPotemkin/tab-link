862b4d15f4b5f6797294735dd35c2150bb17af3e
Pavel Potemkin <p.potemkin@legacystudio.ru>
2022-02-28 16:55:08 +0500

    gh pages

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# tap.link
+# tab-link
 
 ## Project setup
 ```
---
+++ deploy.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env sh
+
+set -e
+
+npm run build
+
+cd dist
+
+git init
+git add -A
+git commit -m 'deploy'
+
+git push -f git@github.com:pavelpotemkin/tab-link.git master:gh-pages
+
+# git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
+
+cd -
