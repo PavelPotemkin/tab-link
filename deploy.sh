7aa19d2c7b40cfda0efdc66fd969abbd063b7015
Pavel Potemkin <p.potemkin@legacystudio.ru>
2022-02-28 18:14:33 +0500

    gh-pages fix

--- deploy.sh
+++ deploy.sh
@@ -10,6 +10,6 @@ git init
 git add -A
 git commit -m 'deploy'
 
-git push -f git@github.com:PavelPotemkin/tab-link.git master:gh-pages
+git push -f git@github.com:PavelPotemkin/tab-link.git main:gh-pages
 
 cd -
